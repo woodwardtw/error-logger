5373e897afa061f20aecd7e93082207123e5df39
woodwardtw <twwoodward@me.com>
2022-10-18 12:37:38 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Plugin Name: Error Logger TEST
+Plugin URI:  https://github.com/
+Description: Just making sure you have something written to your error log
+Version:     1.0
+Author:      Tom Woodward
+Author URI:  http://bionicteaching.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: my-toolset
+
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+write_log('Hi! Your errors are being logged.');
+write_log('your site is at ' . DOMAIN_CURRENT_SITE);
+
+//LOGGER -- like frogger but more useful
+
+if ( ! function_exists('write_log')) {
+   function write_log ( $log )  {
+      if ( is_array( $log ) || is_object( $log ) ) {
+         error_log( print_r( $log, true ) );
+      } else {
+         error_log( $log );
+      }
+   }
+}
+
+  //print("<pre>".print_r($a,true)."</pre>");
